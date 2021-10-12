74a7becb0cddfcc1b8fa1c64812c6c6bc8b9c497
Darren <theinvader360@gmail.com>
2021-10-12 20:09:05 +0100

    introduce pixelgl

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/TheInvader360/sokoban-go
 
 go 1.16
+
+require (
+	github.com/faiface/pixel v0.10.0
+	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d
+)
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+)
+
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Sokoban",
+		Bounds: pixel.R(0, 0, 512, 256),
+		VSync:  true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	win.Clear(colornames.Black)
+
+	for !win.Closed() {
+		win.Update()
+	}
+}
 
 func main() {
-	fmt.Println("Hello Sokoban!")
+	pixelgl.Run(run)
 }
